5537a54e1e18f705262dede0a43aa6d2c2b09011
Raghav Ambashta <raghav.ambashta22@gmail.com>
2022-08-11 16:57:35 +0530

    Added method

--- MyAreas/Class1.cs
+++ MyAreas/Class1.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-
+using System.Collections.Generic;
 namespace MyAreas
 {
     public class Areas
@@ -15,6 +15,29 @@ namespace MyAreas
         public int Rectangle(int l, int b)
         {
             return l * b;
+        }
+
+        public string Login(string user, string pwd)
+        {
+            if (string.IsNullOrEmpty(user) || string.IsNullOrEmpty(pwd))
+            {
+                return "user or pwd is null";
+            }
+            else if (user == "Admin" && pwd == "admin")
+            {
+                return "Welcome Admin";
+            }
+            else return "Invalid user and password";
+        }
+        public List<string> GetNames()
+        {
+            List<string> names = new List<string>();
+            names.Add("Tom");
+            names.Add("Bob");
+            names.Add("Don");
+            names.Add("Thomas");
+            names.Add("Sean");
+            return names;
         }
     }
 }
--- TestMyAreas/UnitTest1.cs
+++ TestMyAreas/UnitTest1.cs
@@ -1,4 +1,5 @@
 using NUnit.Framework;
+using System.Collections.Generic;
 using MyAreas;
 namespace TestMyAreas
 {
@@ -53,11 +54,48 @@ namespace TestMyAreas
         [TestCase(25, 25, ExpectedResult = 630)]
         [TestCase(90, 90, ExpectedResult = 8100)]
         [TestCase(60, 60, ExpectedResult = 3600)]
-
         public int RectangleTest (int l, int b)
         {
             int actresult = areas.Rectangle(l, b);
             return actresult;
+        }
+
+        [Test]
+        public void Login_Null_Message()
+        {
+            string expresult = "username or password is null";
+            string actresult = areas.Login("Tom", null);
+            Assert.AreEqual(expresult, actresult, "Messages not matching");
+        }
+
+        [Test]
+        public void Login_Admin_Success_Message()
+        {
+            string expresult = "Welcome Admin !";
+            string actresult = areas.Login("Admin", "admin");
+            Assert.AreEqual(expresult, actresult, "Messages not proper");
+        }
+
+        [Test]
+        public void Login_Invalid_Message()
+        {
+            //string expresult = "Welcome Admin !";
+            string actresult = areas.Login("Tom", "tom123");
+            Assert.AreEqual("Invalid", actresult);
+        }
+
+        [Test]
+        public void Names_Contains_Null()
+        {
+            List<string> names = areas.GetNames();
+            CollectionAssert.AllItemsAreNotNull(names, "Null values present");
+        }
+
+        [Test]
+        public void Names_are_Unique()
+        {
+            List<string> names = areas.GetNames();
+            CollectionAssert.AllItemsAreUnique(names, "Unique items are NOT present");
         }
     }
 }
